f955c0a6a9abbd7ff1f481e1c93020dcdc00516e
Ala-Hadadi <alahadadi1384@gmail.com>
2024-11-30 12:37:58 -0800

    js9-2

---
+++ B-array-every/exercise.js
@@ -0,0 +1,20 @@
+/*
+   This program should check if the array `group` contains only students
+*/
+
+const students = ["Hossein", "Jamal", "Mehdi", "Shaden", "Zaynab", "Niloofar"];
+const group = ["Niloofar", "Mehdi", "Benyamin", "Zaynab"];
+
+let groupIsOnlyStudents = group.every((item) => {
+  return students.includes(item);
+}); // complete this statement
+
+if (groupIsOnlyStudents) {
+  console.log("The group contains only students");
+} else {
+  console.log("The group does not contain only students");
+}
+
+/* EXPECTED RESULT */
+
+// The group does not contain only students
